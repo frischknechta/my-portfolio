c2a1eaa443c83d8ad5629dbe001bdf1824dd5c36
Adrien Frischknecht <frischknecht.adrien@gmail.com>
2024-04-09 20:50:58 +0200

    Added a new project in Projects section. Modifications and improvements of responsive.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import "./Projects.css";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import Warranty from "../../assets/img/Warranty.png";
 import Marvel from "../../assets/img/Marvel.png";
 import Vinted from "../../assets/img/Vinted.png";
 import Deliveroo from "../../assets/img/Deliveroo.png";
@@ -9,14 +10,49 @@ import Tripadvisor from "../../assets/img/Tripadvisor.png";
 import Airbnb from "../../assets/img/Airbnb.jpg";
 
 const Projects = () => {
-  const [isActive, setIsActive] = useState([0, 0, 0, 0, 0]);
+  const [isActive, setIsActive] = useState([0, 0, 0, 0, 0, 0]);
 
   return (
     <div className="projectsContainer">
       <div
-        className={`vinted ${isActive[0] === 1 ? "active" : "inactive"}`}
+        className={`warranty ${isActive[0] === 1 ? "active" : "inactive"}`}
         onClick={() => {
-          const newTab = [1, 0, 0, 0, 0];
+          const newTab = [1, 0, 0, 0, 0, 0];
+          setIsActive(newTab);
+        }}
+      >
+        <div className="descriptionContainer">
+          <div>
+            <h3>Warranty app</h3>
+            <p>
+              A simple app for warranty registration with reference validation
+              and warranty validity check.
+            </p>
+          </div>
+          <div className="projectLinks">
+            <a
+              href="https://warranty-af.netlify.app/"
+              target="_blank"
+              className="btnLink"
+            >
+              <FontAwesomeIcon icon="fa-desktop" /> DEMO
+            </a>
+            <a
+              href="https://github.com/frischknechta/warranty-app-frontend"
+              target="_blank"
+              className="btnLink"
+            >
+              <FontAwesomeIcon icon="fa-brands fa-github" />
+            </a>
+          </div>
+          <div className="descriptionBackground"></div>
+        </div>
+        <img src={Warranty} alt="Warranty registration app" />
+      </div>
+      <div
+        className={`vinted ${isActive[1] === 1 ? "active" : "inactive"}`}
+        onClick={() => {
+          const newTab = [0, 1, 0, 0, 0, 0];
           setIsActive(newTab);
         }}
       >
@@ -46,9 +82,9 @@ const Projects = () => {
         <img src={Vinted} alt="Vinted homepage" />
       </div>
       <div
-        className={`marvel ${isActive[1] === 1 ? "active" : "inactive"}`}
+        className={`marvel ${isActive[2] === 1 ? "active" : "inactive"}`}
         onClick={() => {
-          const newTab = [0, 1, 0, 0, 0];
+          const newTab = [0, 0, 1, 0, 0, 0];
           setIsActive(newTab);
         }}
       >
@@ -80,9 +116,9 @@ const Projects = () => {
         <img src={Marvel} alt="Marvel homepage" />
       </div>
       <div
-        className={`airbnb ${isActive[2] === 1 ? "active" : "inactive"}`}
+        className={`airbnb ${isActive[3] === 1 ? "active" : "inactive"}`}
         onClick={() => {
-          const newTab = [0, 0, 1, 0, 0];
+          const newTab = [0, 0, 0, 1, 0, 0];
           setIsActive(newTab);
         }}
       >
@@ -112,9 +148,9 @@ const Projects = () => {
         <img src={Tripadvisor} alt="Tripadvisor homepage" />
       </div>
       <div
-        className={`deliveroo ${isActive[3] === 1 ? "active" : "inactive"}`}
+        className={`deliveroo ${isActive[4] === 1 ? "active" : "inactive"}`}
         onClick={() => {
-          const newTab = [0, 0, 0, 1, 0];
+          const newTab = [0, 0, 0, 0, 1, 0];
           setIsActive(newTab);
         }}
       >
@@ -146,9 +182,9 @@ const Projects = () => {
         <img src={Deliveroo} alt="Deliveroo homepage" />
       </div>
       <div
-        className={`tripadvisor ${isActive[4] === 1 ? "active" : "inactive"}`}
+        className={`tripadvisor ${isActive[5] === 1 ? "active" : "inactive"}`}
         onClick={() => {
-          const newTab = [0, 0, 0, 0, 1];
+          const newTab = [0, 0, 0, 0, 0, 1];
           setIsActive(newTab);
         }}
       >
