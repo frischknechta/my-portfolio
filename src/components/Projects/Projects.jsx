2a1dbb00acea5e367fe79c9d2b1998b21e170d36
Adrien Frischknecht <frischknecht.adrien@gmail.com>
2024-07-29 22:06:53 +0200

    Modification of project section to add blog and remove tripadvisor. Added scroll-animations on experience section.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,11 +2,11 @@ import "./Projects.css";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import Blog from "../../assets/img/blog.png";
 import Warranty from "../../assets/img/Warranty.png";
 import Marvel from "../../assets/img/Marvel.png";
 import Vinted from "../../assets/img/Vinted.png";
 import Deliveroo from "../../assets/img/Deliveroo.png";
-import Tripadvisor from "../../assets/img/Tripadvisor.png";
 import Airbnb from "../../assets/img/Airbnb.jpg";
 
 const Projects = () => {
@@ -15,9 +15,45 @@ const Projects = () => {
   return (
     <div className="projectsContainer">
       <div
-        className={`warranty ${isActive[0] === 1 ? "active" : "inactive"}`}
+        className={`blog ${isActive[0] === 1 ? "active" : "inactive"}`}
         onClick={() => {
           const newTab = [1, 0, 0, 0, 0, 0];
+          setIsActive(newTab);
+        }}
+      >
+        <div className="descriptionContainer">
+          <div>
+            <h3>Blog</h3>
+            <p>
+              A blog developped with Next.js, Typescript and Tailwind CSS.
+              Authentication is managed with Auth.js, pictures are stored on
+              cloudinary and blog articles are saved in a MongoDB database.
+            </p>
+          </div>
+          <div className="projectLinks">
+            <a
+              href="https://next-blog-af.vercel.app/"
+              target="_blank"
+              className="btnLink"
+            >
+              <FontAwesomeIcon icon="fa-desktop" /> DEMO
+            </a>
+            <a
+              href="hhttps://github.com/frischknechta/next-blog"
+              target="_blank"
+              className="btnLink"
+            >
+              <FontAwesomeIcon icon="fa-brands fa-github" />
+            </a>
+          </div>
+          <div className="descriptionBackground"></div>
+        </div>
+        <img src={Blog} alt="Warranty registration app" />
+      </div>
+      <div
+        className={`warranty ${isActive[1] === 1 ? "active" : "inactive"}`}
+        onClick={() => {
+          const newTab = [0, 1, 0, 0, 0, 0];
           setIsActive(newTab);
         }}
       >
@@ -50,9 +86,9 @@ const Projects = () => {
         <img src={Warranty} alt="Warranty registration app" />
       </div>
       <div
-        className={`vinted ${isActive[1] === 1 ? "active" : "inactive"}`}
+        className={`vinted ${isActive[2] === 1 ? "active" : "inactive"}`}
         onClick={() => {
-          const newTab = [0, 1, 0, 0, 0, 0];
+          const newTab = [0, 0, 1, 0, 0, 0];
           setIsActive(newTab);
         }}
       >
@@ -82,9 +118,9 @@ const Projects = () => {
         <img src={Vinted} alt="Vinted homepage" />
       </div>
       <div
-        className={`marvel ${isActive[2] === 1 ? "active" : "inactive"}`}
+        className={`marvel ${isActive[3] === 1 ? "active" : "inactive"}`}
         onClick={() => {
-          const newTab = [0, 0, 1, 0, 0, 0];
+          const newTab = [0, 0, 0, 1, 0, 0];
           setIsActive(newTab);
         }}
       >
@@ -114,38 +150,6 @@ const Projects = () => {
           <div className="descriptionBackground"></div>
         </div>
         <img src={Marvel} alt="Marvel homepage" />
-      </div>
-      <div
-        className={`airbnb ${isActive[3] === 1 ? "active" : "inactive"}`}
-        onClick={() => {
-          const newTab = [0, 0, 0, 1, 0, 0];
-          setIsActive(newTab);
-        }}
-      >
-        <div className="descriptionContainer">
-          <div>
-            <h3>Tripadvisor</h3>
-            <p>A reproduction of a city page on Tripavisor.</p>
-          </div>
-          <div className="projectLinks">
-            <a
-              href="https://mytripadvisor-berlin.netlify.app/"
-              target="_blank"
-              className="btnLink"
-            >
-              <FontAwesomeIcon icon="fa-desktop" /> DEMO
-            </a>
-            <a
-              href="https://github.com/frischknechta/TripAdvisor-front"
-              target="_blank"
-              className="btnLink"
-            >
-              <FontAwesomeIcon icon="fa-brands fa-github" />
-            </a>
-          </div>
-          <div className="descriptionBackground"></div>
-        </div>
-        <img src={Tripadvisor} alt="Tripadvisor homepage" />
       </div>
       <div
         className={`deliveroo ${isActive[4] === 1 ? "active" : "inactive"}`}
@@ -182,7 +186,7 @@ const Projects = () => {
         <img src={Deliveroo} alt="Deliveroo homepage" />
       </div>
       <div
-        className={`tripadvisor ${isActive[5] === 1 ? "active" : "inactive"}`}
+        className={`airbnb ${isActive[5] === 1 ? "active" : "inactive"}`}
         onClick={() => {
           const newTab = [0, 0, 0, 0, 0, 1];
           setIsActive(newTab);
